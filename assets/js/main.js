335f05fd74dd35060f2e2e0dc9badcfa7d45e832
Jacob Zamano <zamano.jacob@gmail.com>
2019-06-25 08:37:11 -0700

    adding files!

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+document.onreadystatechange = function () {
+    var state = document.readyState
+    if (state == 'interactive') {
+         document.getElementById('contents').style.visibility="hidden";
+    } else if (state == 'complete') {
+        setTimeout(function(){
+           document.getElementById('interactive');
+           document.getElementById('load').style.visibility="hidden";
+           AOS.init();
+           document.getElementById('contents').style.visibility="visible";
+        },1000);
+    }
+    $(function() {
+        $('a[href*=#]').on('click', function(e) {
+          e.preventDefault();
+          $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+        });
+      });
+}
